aec7774069216cfed2bc444c1d0616d04ec5b737
Your Name <you@example.com>
2024-07-01 09:35:51 +0200

    #4Panier

--- produits.js
+++ produits.js
@@ -19,12 +19,12 @@ function listeProduits (data){
     nomElement.innerText=article.nom
     const prixElement=document.createElement('p')
     prixElement.innerText=`${article.prix} €`
-    // const cateElement=document.createElement('p')
-    // cateElement.innerText=`Catégorie : ${article.catégorie}`
+    
 
     const addToCart=document.createElement('button')
     addToCart.innerText="Ajouter au panier"
     addToCart.className="add-to-cart"
+    
     const imageToCart=document.createElement('img')
     imageToCart.src= "image/cart3.svg"
     imageToCart.id='img-caddie'
@@ -36,16 +36,30 @@ function listeProduits (data){
     articleElement.appendChild(imageElement)
     articleElement.appendChild(nomElement)
     articleElement.appendChild(prixElement)
-    // articleElement.appendChild(cateElement)
     articleElement.appendChild(addToCart)
     addToCart.appendChild(imageToCart)
 
    
-     
- 
+    const contenuePanier=document.querySelector('.pop-up-panier')
     
-    imageToCart.addEventListener('click', ()=>{
-        alert(`Je veux acheter ${article.nom} à ${article.prix} €`)
+    
+    addToCart.addEventListener('click', ()=>{
+        // alert(`Je veux acheter ${article.nom} à ${article.prix} €`)
+    const nomPanier = document.createElement('p')
+    nomPanier.innerText=article.nom
+    const boutonPlus = document.createElement('button')
+    boutonPlus.innerText="+"
+    boutonPlus.className="bouton-plus"
+    const boutonMoins = document.createElement('button')
+    boutonMoins.innerText="-"
+    boutonMoins.className="bouton-moins"
+    const prixPanier= document.createElement('p')
+    prixPanier.innerText = `${article.prix}€`
+
+    contenuePanier.appendChild(nomPanier)
+    contenuePanier.appendChild(prixPanier)
+    prixPanier.appendChild(boutonMoins)
+    prixPanier.appendChild(boutonPlus)
     })
    }
    
@@ -111,6 +125,21 @@ function listeProduits (data){
         btnPanier.textContent=currentValue
     })
    })
+
+
+const boutonClose= document.getElementById('viderPanier')
+boutonClose.addEventListener('click',()=>{
+    
+})
+
+
+
+
+
+
+
+   
+
 
 
 
